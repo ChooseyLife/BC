c2c214c3e96ee18b60919dcf6e5b69bce2245545
e <563798627@qq.com>
2018-03-26 14:16:57 +0800

    add less module

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div class="header-container">
+    <div class="header-logo">
+      <img src="../../assets/logo.png">
+    </div>
+    <div class="header-name">
+      <span>集美台商服务中心-能耗监管平台</span>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -7,5 +14,23 @@
 </script>
 
 <style lang="less" scoped>
-
+@import "../../common/less/variable.less";
+.header-container{
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+    .header-logo{
+      display: inline-block;
+      font-size: 0;
+      width: 6rem;
+      height: 6rem;
+        img{
+          .img
+        }
+    }
+    .header-name{
+      display: inline-block;
+    }
+}
 </style>
