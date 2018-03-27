cccfb4493a7428039c0bee2d080a3931bb1110c1
e <563798627@qq.com>
2018-03-27 17:53:12 +0800

    replace logo

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,31 +1,14 @@
 <template>
   <div class="header-container">
-    <div class="header-logo">
-      <img src="../../assets/logo.png">
+    <div class="header-content">
+      <div class="header-logo">
+        <img src="../../assets/logo.png">
+      </div>
+      <div class="header-name">
+        <span>集美台商服务中心-能耗监管平台</span>
+      </div>
     </div>
-    <div class="header-name">
-      <span>集美台商服务中心-能耗监管平台</span>
-    </div>
-    <div class="header-wather">
-      <ul>
-        <li class="item">
-          <div class="item-img">
-            <img src="../../assets/logo.png" alt="sign">
-          </div>
-          <div class="item-text">
-            <h2 class="text-title">温度：32℃</h2>
-            <p class="text-details">湿度：58%</p>
-          </div>
-        </li>
-      </ul>
-    </div>
-    <div class="header-qx">
-      <span>admin</span>
-      <span>退出</span>
-    </div>
-    <div>
-      <i-menu></i-menu>
-    </div>
+    <i-menu></i-menu>
   </div>
 </template>
 
@@ -50,54 +33,23 @@ export default {
   height: 100%;
   background: @color-background-d;
   color: @color-text-line-d;
-    .header-logo{
-      float: left;
-      font-size: 0;
-      width: 6rem;
-      height: 6rem;
+    .header-content{
+      overflow: hidden;
+      .header-logo{
+        float: left;
+        padding: 0.5rem;
+        font-size: 0;
+        width: 6rem;
+        height: 6rem;
         img{
           .img
         }
-    }
-    .header-name{
-      display: inline-block;
-      padding: 1rem;
-      height: 6rem;
-    }
-    .header-qx{
-      float: right;
-      display: inline-block;
-      padding: 1rem;
-      height: 6rem;
-    }
-    .header-wather {
-      float: right;
-      padding: 1rem;
-      color:  @color-text-dd;
-      .item {
-        display:flex;
-        box-sizing:border-box;
-        padding:0 20px 0 20px
       }
-      .item-img{
-        flex:0 0 60px;
-        padding-right:20px;
-        width:60px
-      }
-      .item-text{
-        display:flex;
-        flex-direction:column;
-        justify-content:center;
-        flex:1;
-        line-height:20px;
-        overflow:hidden;
-        font-size:14px;
-      }
-      .text-title{
-        margin-bottom:10px;
-      }
-      img{
-        .img
+      .header-name{
+        display: inline-block;
+        padding-top: 2.5rem;
+        height: 6rem;
+        font-size: 1.2rem;
       }
     }
 }
