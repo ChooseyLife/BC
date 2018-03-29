fb90304832988915d18bbf0e301876f5197ffc21
e <563798627@qq.com>
2018-03-29 17:32:10 +0800

    add Menu

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -1,5 +1,6 @@
 <template>
-    <div></div>
+    <aside class="menu">
+    </aside>
 </template>
 
 <script type="text/ecmascript-6">
@@ -7,5 +8,12 @@
 </script>
 
 <style lang="less" scoped>
-
+.menu {
+  position: fixed;
+  top: 4rem;
+  left: 0;
+  width: 180px;
+  height: 100%;
+  box-shadow: 0 2px 3px rgba(17, 17, 17, 0.1), 0 0 0 1px rgba(17, 17, 17, 0.1);
+}
 </style>
